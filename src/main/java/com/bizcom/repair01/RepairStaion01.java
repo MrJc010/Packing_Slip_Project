fbc04de774a148fe5325f0aec5fcfc7901af9f82
Desktop_Home <52801407+mlucario88@users.noreply.github.com>
2020-03-25 16:50:15 -0700

    add new feature  fix repair

--- src/main/java/com/bizcom/repair01/RepairStaion01.java
+++ src/main/java/com/bizcom/repair01/RepairStaion01.java
@@ -1,6 +1,7 @@
 package com.bizcom.repair01;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,6 +23,10 @@ public class RepairStaion01 extends HttpServlet {
 	private int currentRev = -1;
 	private int maxRev = 6;
 
+	private final String MICI = "MICI";
+	private final String REPAIR01_FAIL = "REPAIR01_FAIL";
+	private final String REPAIR01 = "REPAIR01";
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -33,31 +38,40 @@ public class RepairStaion01 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String action = request.getParameter("action01");
-		System.out.println("go in " + action);
+
 		request.setAttribute("setRepair01Hidden", "hidden");
 		request.setAttribute("setRepair01HiddenError", "hidden");
 
 		if (action != null) {
 			switch (action) {
 			case "findPPID":
+
 				ppid = request.getParameter("inputppid");
-				if (!ppid.isEmpty()) {
-					request.setAttribute("setPPID", ppid);
-					String curRev = db.getCurrentRev(ppid);
-					currentRev = Integer.parseInt(curRev.substring(curRev.length() - 2, curRev.length()));
+				if (!ppid.isEmpty() && db.isPPIDExistInMICI(ppid)) {
 
-					System.out.println("currentRev : " + currentRev);
-					if (currentRev >= maxRev) {
+					String[] stationIndo = db.getCurrentStation(ppid);
 
-						request.setAttribute("setRepair01Hidden", "hidden");
-						request.getRequestDispatcher("/WEB-INF/views/repair_01/repair01.jsp").forward(request,
-								response);
+					// Check If PPID stay at corrected station
+					// FROM : MICI TO : REPAIR01_FAIL
+					if (stationIndo[0].equalsIgnoreCase("MICI") && stationIndo[1].equalsIgnoreCase(REPAIR01_FAIL)) {
+						boolean updateFlag = db.generateErrorRecord(ppid);
+
+						if (updateFlag) {
+							// Update status also
+							db.updateCurrentStation(MICI, REPAIR01, ppid);
+							updateUI(request, response, ppid);
+
+						} else {
+							displayError(request, response, ppid, "Error system! We cannot upload error to system.");
+						}
+
 					} else {
-						updateRevision(request, response, ppid);
+						getErrors(request, response, ppid);
+						updateUI(request, response, ppid);
 					}
+
 				} else {
-					request.setAttribute("setRepair01HiddenError", "show");
-					request.setAttribute("setErrorMessage", "PPID is Empty or Not Found!");
+					displayError(request, response, ppid, "PPID is Empty or Not Found!");
 				}
 				break;
 			case "updateRevision":
@@ -85,6 +99,21 @@ public class RepairStaion01 extends HttpServlet {
 				db.updateRevision(ppid, newRev);
 				doGet(request, response);
 				break;
+
+			case "SubmitAction":
+				String err = request.getParameter("errorValueAction");
+				String errorCode = err.split(" --> ")[0];
+				String duty = request.getParameter("duty" + err);
+				String oldPN = request.getParameter("oldPN" + err);
+				String newPN = request.getParameter("newPN" + err);
+				String area = request.getParameter("area" + err);
+				String actionJob = request.getParameter("action" + err);
+				db.updateRepair01RecordAction(errorCode, ppid, duty, oldPN, newPN, area, actionJob);
+				getErrors(request, response, ppid);
+				updateUI(request, response, ppid);
+//				request.getRequestDispatcher("/WEB-INF/views/repair_01/repair01.jsp").forward(request, response);				
+				break;
+
 			}
 
 		}
@@ -130,6 +159,7 @@ public class RepairStaion01 extends HttpServlet {
 			// error code
 
 		} else {
+			request.setAttribute("setRepair01HiddenError", "hidden");
 			request.getRequestDispatcher("/WEB-INF/views/repair_01/repair01.jsp").forward(request, response);
 		}
 
@@ -144,17 +174,60 @@ public class RepairStaion01 extends HttpServlet {
 	}
 
 	public void getErrors(HttpServletRequest request, HttpServletResponse response, String ppid) {
+		// improve later
 		List<String> errorList = db.fetchErrorForRepair01FromMICI(ppid);
+		// try to fetch in
+		List<String> undoneList = db.getAllUndoneErrorCode(ppid);
+		List<String> errorListModifed = new ArrayList<>();
+		if (undoneList.isEmpty()) {
+			errorList.clear();
+			errorListModifed.clear();
+		}
+
+		if (errorList.size() > 0) {
+			for (String s : errorList) {
+				String tempErroCode = s.split(" --> ")[0];
+
+				if (undoneList.contains(tempErroCode)) {
+					errorListModifed.add(s);
+				}
+			}
+		}
+
 		request.setAttribute("setRepair01HiddenFix", "show");
-		if (errorList.size() == 0) {
+		if (errorListModifed.size() == 0) {
 			request.setAttribute("setRepair01HiddenError", "");
-			request.setAttribute("setErrorMessage", ppid + " not found at this station.");
+			request.setAttribute("setSuccessMessage",
+					ppid + " has NO ERROR! Please check REVESION UPGRADE IF REQUIRE!");
 		} else {
 			// List all error
-			request.setAttribute("sizeErrorList", errorList.size());
+			request.setAttribute("sizeErrorList", errorListModifed.size());
 			request.setAttribute("setRepair01Hidden", "");
-			request.setAttribute("errorList", errorList);
+			request.setAttribute("errorList", errorListModifed);
 
 		}
+	}
+
+	public void updateUI(HttpServletRequest request, HttpServletResponse response, String ppid)
+			throws ServletException, IOException {
+		request.setAttribute("setPPID", ppid);
+		String curRev = db.getCurrentRev(ppid);
+		currentRev = Integer.parseInt(curRev.substring(curRev.length() - 2, curRev.length()));
+
+		if (currentRev >= maxRev) {
+
+			request.setAttribute("setRepair01Hidden", "hidden");
+			request.getRequestDispatcher("/WEB-INF/views/repair_01/repair01.jsp").forward(request, response);
+		} else {
+			updateRevision(request, response, ppid);
+		}
+	}
+
+	public void displayError(HttpServletRequest request, HttpServletResponse response, String ppid, String message)
+			throws ServletException, IOException {
+		request.setAttribute("setRepair01HiddenError", "show");
+		request.setAttribute("setPPID", ppid);
+		request.setAttribute("setErrorMessage", message);
+		request.getRequestDispatcher("/WEB-INF/views/repair_01/repair01.jsp").forward(request, response);
 	}
 }
