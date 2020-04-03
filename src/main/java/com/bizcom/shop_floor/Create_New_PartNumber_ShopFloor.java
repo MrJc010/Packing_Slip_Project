676f0ddaffbfa4650e2c1594ea52376274882860
Desktop_Home <52801407+mlucario88@users.noreply.github.com>
2020-04-03 12:02:28 -0700

    fix name jsp file

--- src/main/java/com/bizcom/shop_floor/Create_New_PartNumber_ShopFloor.java
+++ src/main/java/com/bizcom/shop_floor/Create_New_PartNumber_ShopFloor.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/shopfloor/create_new_partnumber")
+@WebServlet("/shopfloor/create_new_partnumber_step1")
 public class Create_New_PartNumber_ShopFloor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -15,7 +15,7 @@ public class Create_New_PartNumber_ShopFloor extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		request.getRequestDispatcher("/WEB-INF/views/shoop_floor/create_new_project_step_1.jsp").forward(request,
+		request.getRequestDispatcher("/WEB-INF/views/shoop_floor/create_new_part_number_step_1.jsp").forward(request,
 				response);
 	}
 
@@ -23,17 +23,14 @@ public class Create_New_PartNumber_ShopFloor extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		System.out.println("CALL doPost create_new_partnumber_step1");
 		String pn = request.getParameter("partnumber");
 		String model = request.getParameter("model");
 		String desc = request.getParameter("description");
 		String url = "";
-		if(!pn.isEmpty() && !model.isEmpty() && desc.isEmpty()) {
-			url = "/shopfloor/create_new_locations?pn=" + pn + "&model=" + model + "&desc=" + desc;	
-			response.sendRedirect(request.getContextPath() + url);
-		}
-		else {
-			// pop up warnring
-		}
-		
+
+		url = "/shopfloor/create_new_partnumber_step2?pn=" + pn + "&model=" + model + "&desc=" + desc;
+		response.sendRedirect(request.getContextPath() + url);
+
 	}
 }
